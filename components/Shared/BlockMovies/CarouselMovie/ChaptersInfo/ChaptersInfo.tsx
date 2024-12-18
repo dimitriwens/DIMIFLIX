64b2ff90005c68e850a6a521325865fa9c725053
dimitriwens <dimitriantonakos@gmail.com>
2024-12-18 11:42:33 +0100

    fixed images profile page

--- components/Shared/BlockMovies/CarouselMovie/ChaptersInfo/ChaptersInfo.tsx
+++ components/Shared/BlockMovies/CarouselMovie/ChaptersInfo/ChaptersInfo.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import { ChaptersInfoProps } from "./ChaptersInfo.types";
 
 export function ChaptersInfo(props: ChaptersInfoProps) {
-  const { age, duration } = props;
+  const { age, duration, genre } = props;
 
   const colorByAge = (age: string) => {
     if (age === "0") {
@@ -20,19 +20,33 @@ export function ChaptersInfo(props: ChaptersInfoProps) {
   };
 
   return (
-    <div className="flex gap-1 mb-3">
-      <p
-        className={cn(
-          "text-white w-fit rounded-md font-semibold text-[10px] h-fit py-0.5 px-1",
-          colorByAge(age)
-        )}
-      >
-        +{age}
-      </p>
-      <p className="text-slate-50 text-[12px] flex items-center">{duration}</p>
-      <p className="border-[1px] border-gray-400 px-2 text-[0.5rem] text-gray-400 flex items-center rounded-md h-[15px]">
-        HD
-      </p>
-    </div>
+    <section>
+      <div className="flex gap-3 mb-3 items-center">
+        <p
+          className={cn(
+            "text-white w-fit rounded-md font-semibold text-[10px] h-fit py-0.5 px-1",
+            colorByAge(age)
+          )}
+        >
+          +{age}
+        </p>
+        <p className="text-slate-50 text-[12px] flex items-center">
+          {duration}
+        </p>
+        <p className="border-[1px] border-gray-400 px-2 text-[0.5rem] text-gray-400 flex items-center rounded-md h-[15px]">
+          HD
+        </p>
+      </div>
+      <div className="flex flex-wrap gap-3 text-slate-50 text-[12px] items-center">
+        {genre.map((genre) => (
+          <p
+            key={genre}
+            className="text-slate-50 text-[12px] flex items-center flex-row"
+          >
+            {genre}
+          </p>
+        ))}
+      </div>
+    </section>
   );
 }
